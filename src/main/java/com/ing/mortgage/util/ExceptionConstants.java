8ea019222881aec578368d340843fc907665f829
priyanka-gupta1998 <55982671+priyanka-gupta1998@users.noreply.github.com>
2019-10-10 23:01:09 +0530

    Update ExceptionConstants.java

--- src/main/java/com/ing/mortgage/util/ExceptionConstants.java
+++ src/main/java/com/ing/mortgage/util/ExceptionConstants.java
@@ -4,6 +4,6 @@ public class ExceptionConstants {
 	
 	public static final String USER_NOT_FOUND = "Invalid credentials";
 	public static final String ACCOUNT_NOT_FOUND = "No accounts found..";
-
+        public static final String CUSTOMER_NOT_PRESENT="CustomerId is not present";
 	
 }
