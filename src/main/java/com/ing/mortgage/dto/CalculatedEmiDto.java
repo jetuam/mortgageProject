5bf57458de068f737de4bc65859f898d21f896d5
priyanka-gupta1998 <55982671+priyanka-gupta1998@users.noreply.github.com>
2019-10-10 19:50:14 +0530

    Update CalculatedEmiDto.java

--- src/main/java/com/ing/mortgage/dto/CalculatedEmiDto.java
+++ src/main/java/com/ing/mortgage/dto/CalculatedEmiDto.java
@@ -3,11 +3,22 @@ package com.ing.mortgage.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+
 public class CalculatedEmiDto {
 
 	private Double emiAmount;
 	private int statusCode;
+	public Double getEmiAmount() {
+		return emiAmount;
+	}
+	public void setEmiAmount(Double emiAmount) {
+		this.emiAmount = emiAmount;
+	}
+	public int getStatusCode() {
+		return statusCode;
+	}
+	public void setStatusCode(int statusCode) {
+		this.statusCode = statusCode;
+	}
 	
 }
