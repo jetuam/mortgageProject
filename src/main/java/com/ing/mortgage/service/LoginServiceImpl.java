7ad3efc5c9268110f33be11bb08ba4553b5b64e9
priyanka-gupta1998 <55982671+priyanka-gupta1998@users.noreply.github.com>
2019-10-10 23:00:01 +0530

    Update LoginServiceImpl.java

--- src/main/java/com/ing/mortgage/service/LoginServiceImpl.java
+++ src/main/java/com/ing/mortgage/service/LoginServiceImpl.java
@@ -2,10 +2,8 @@ package com.ing.mortgage.service;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.ing.mortgage.dto.RequestLoanDto;
 import com.ing.mortgage.dto.RequestLoginDto;
 import com.ing.mortgage.entity.Customer;
@@ -25,24 +23,35 @@ public class LoginServiceImpl implements LoginService {
 	CustomerRepository customerRepository;
 	
 public List<RequestLoanDto> loanDetails(RequestLoginDto requestLoginDto)
-{    List<RequestLoanDto> loanAccountDetails=new ArrayList<>();
+{   
+	List<RequestLoanDto> loanAccountDetails=new ArrayList<>();
 	Customer customerDetails=customerRepository.findByEmailIdAndPassword(requestLoginDto.getEmailId(),
 			requestLoginDto.getPassword());
-	Long customerId=customerDetails.getCustomerId();
-	List<Loan> loanDetails=loanRepository.findAllById(customerId);
-	if(loanDetails!=null){
-	loanDetails.forEach(loan->{
-		RequestLoanDto requestLoanDto=new RequestLoanDto();
-		requestLoanDto.setLoanAccountNo(loan.getLoanAccountNo());
-		requestLoanDto.setCustomerId(loan.getCustomer().getCustomerId());
-		loanAccountDetails.add(requestLoanDto);
+	if(customerDetails!=null)
+	{
+	 Long customerId=customerDetails.getCustomerId();
+	 List<Loan> loanDetails=loanRepository.findAllById(customerId);
+	  if(loanDetails!=null){
+	     loanDetails.forEach(loan->{
+		 RequestLoanDto requestLoanDto=new RequestLoanDto();
+		 requestLoanDto.setLoanAccountNo(loan.getLoanAccountNo());
+		 requestLoanDto.setCustomerId(loan.getCustomer().getCustomerId());
+		 loanAccountDetails.add(requestLoanDto);
 		});
 	return loanAccountDetails;
 	}
+	
 	else
 	{
 		
 		throw new CommonException(ExceptionConstants.USER_NOT_FOUND);
+		
+	}
+	
+	}
+	else
+	{
+		throw new CommonException(ExceptionConstants.CUSTOMER_NOT_PRESENT);
 		
 	}
 }	
