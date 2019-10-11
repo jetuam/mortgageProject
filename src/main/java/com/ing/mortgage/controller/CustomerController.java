41ad4e316ddfd43bca4b23910d811ad28dc66db3
user1 <user1@LP-CNU4129QG2.HCLT.CORP.HCL.IN>
2019-10-11 12:49:04 +0530

    featured login and scheduler

--- src/main/java/com/ing/mortgage/controller/CustomerController.java
+++ src/main/java/com/ing/mortgage/controller/CustomerController.java
@@ -3,6 +3,7 @@ package com.ing.mortgage.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import com.ing.mortgage.service.EmiCalculationServiceImpl;
 
 @RestController
 @RequestMapping("/mortgage")
+@CrossOrigin(allowedHeaders = { "*", "*/" }, origins = { "*", "*/" })
 public class CustomerController {
 
 	
